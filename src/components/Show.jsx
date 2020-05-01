9a454e02b8c867d5b57048caf0b49ccd5be2f3c1
justin0979 <justinm0979@gmail.com>
2020-05-01 18:19:42 -0500

    change lazy load text

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <div>
-      <h1>This should lazy load</h1>
+      <h1>This should lazy load and well === true</h1>
     </div>
   );
 };
